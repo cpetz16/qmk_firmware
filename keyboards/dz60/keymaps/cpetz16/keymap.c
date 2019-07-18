8d7080eb3d3c495bf4b117397b42b6a2490be837
Conor Petzold <conorpetz@gmail.com>
2019-07-18 13:18:57 -0400

    Cleaned up keymap.c file, added information to README.md

--- keyboards/dz60/keymaps/cpetz16/keymap.c
+++ keyboards/dz60/keymaps/cpetz16/keymap.c
@@ -1,50 +1,30 @@
 #include QMK_KEYBOARD_H
 
+//TASK chains the keypresses needed to open task manager into a single key press
+#define TASK (LCTL(LALT(KC_DEL)))
 #define _____ KC_TRNS
+
+enum layers {
+    _BL = 0, //Base Layer
+    _FL,     //Function Layer
+    _SL      //Settings Layer
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /* Qwerty
-   * ,-----------------------------------------------------------------------------------------.
-   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bkspc   |
-   * |-----------------------------------------------------------------------------------------+
-   * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
-   * |-----------------------------------------------------------------------------------------+
-   * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
-   * |-----------------------------------------------------------------------------------------+
-   * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | DEL |
-   * |-----------------------------------------------------------------------------------------+
-   * | Ctrl |  Cmd  |  Alt  |              Space                | RAlt | FN  |  L  |  D  |  R  |
-   * `-----------------------------------------------------------------------------------------'
-   */
-
-  LAYOUT_directional(
+  [_BL] = LAYOUT_directional(
       KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC,
 
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
 
       KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
 
-      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MO(1), KC_UP, LCTL(LALT(KC_DEL)),
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, MO(1), KC_UP, TASK,
 
       KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_MENU, MO(2), KC_LEFT, KC_DOWN, KC_RIGHT
       ),
 
-  /* FN1 Layer
-   * ,-----------------------------------------------------------------------------------------.
-   * | Esc |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |   RESET   |
-   * |-----------------------------------------------------------------------------------------+
-   * |        |RBB T|RGB M| Hue+| Hue-| Sat+| Sat-| Val+| Val-|     |     |      |      |      |
-   * |-----------------------------------------------------------------------------------------+
-   * |         | BL T| BL M| BL+ | BL- |     |     |     |     |     |     |     |             |
-   * |-----------------------------------------------------------------------------------------+
-   * |           |     |     |     |     |     |     |     |     |     |     |     |     |     |
-   * |-----------------------------------------------------------------------------------------+
-   * |      |       |       |                                   |     |      |     |     |     |
-   * `-----------------------------------------------------------------------------------------'
-   */
-
-  LAYOUT_directional(
+  [_FL] = LAYOUT_directional(
       KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _____, _____,
 
       _____, KC_MPRV, KC_MNXT, _____, _____, _____, _____, _____, _____, _____, KC_PGUP, KC_PGDN, _____, _____,
@@ -56,21 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _____, _____, KC_MUTE, KC_MPLY, KC_MPLY, KC_MPLY, KC_MYCM, _____, _____, _____, _____
       ),
 
-  /* FN2 Layer
-   * ,-----------------------------------------------------------------------------------------.
-   * | Esc |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |   RESET   |
-   * |-----------------------------------------------------------------------------------------+
-   * |        |RBB T|RGB M| Hue+| Hue-| Sat+| Sat-| Val+| Val-|     |     |      |      |      |
-   * |-----------------------------------------------------------------------------------------+
-   * |         | BL T| BL M| BL+ | BL- |     |     |     |     |     |     |     |             |
-   * |-----------------------------------------------------------------------------------------+
-   * |           |     |     |     |     |     |     |     |     |     |     |     |     |     |
-   * |-----------------------------------------------------------------------------------------+
-   * |      |       |       |                                   |     |      |     |     |     |
-   * `-----------------------------------------------------------------------------------------'
-   */
-
-   LAYOUT_directional(
+   [_SL] = LAYOUT_directional(
     _____, RGB_TOG, RGB_MOD, RGB_RMOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD, _____, _____, _____,
 
     _____, BL_TOGG, BL_INC, BL_DEC, BL_BRTG, _____, _____, _____, _____, _____, _____, _____, _____, _____,
@@ -82,3 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RESET, _____, _____, _____, _____, _____, _____, KC_TRNS, _____, _____, _____
     )
 };
+
+/* function that can be used to set solid underglow color
+ * this example changes all underglow lights to yellow
+ * the list of colors can be found in qmk_firmware/quantum/rgblight_list.h
+void matrix_init_user(void) {
+    rgblight_sethsv_noeeprom_yellow();
+  }
+*/
